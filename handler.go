244899849dd72815207b7e4973353fb1bfa5c23d
keremdokumaci <kerem.dokumaci@yemeksepeti.com>
2022-09-10 19:37:59 +0300

    add: html template basics

--- handler.go
+++ handler.go
@@ -1,12 +1,37 @@
 package comandante
 
 import (
+	"html/template"
 	"net/http"
 
 	filemanager "github.com/keremdokumaci/comandante/src/file_manager"
 )
 
+type envVar struct {
+	Key   string
+	Value string
+}
+type htmlData struct {
+	EnvVars []envVar
+}
+
 func HandlerFunc(w http.ResponseWriter, r *http.Request) {
+	htmlData := htmlData{}
+	envVars := filemanager.ReadConfigurationJson()
+	for key, value := range envVars {
+		envVar := envVar{
+			Key:   key,
+			Value: value,
+		}
+		htmlData.EnvVars = append(htmlData.EnvVars, envVar)
+	}
+
 	w.Header().Add("Content Type", "text/plain")
-	w.Write([]byte(filemanager.ReadHtml()))
+	t, err := template.New("comandante").Parse(filemanager.ReadHtml())
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	t.Execute(w, htmlData)
 }
